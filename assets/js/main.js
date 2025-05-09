56d3f0712ac33b93b59aba4b59365aa52b661699
LAWANI-crypto <abdulakanmu1@gmail.com>
2025-05-09 12:13:23 +0100

    fix:verification

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -50,7 +49,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
+
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
